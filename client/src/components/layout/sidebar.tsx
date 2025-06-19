08d627e2b42793eb86368ff20d43810e2f11df13
gnavasardyan <g.navasardyan@equiron.com>
2025-06-19 09:23:21 +0300

    Changes to be committed: 	modified:   client/src/components/layout/sidebar.tsx

--- client/src/components/layout/sidebar.tsx
+++ client/src/components/layout/sidebar.tsx
@@ -84,24 +84,24 @@ export function Sidebar() {
         <nav className="py-4 flex-grow">
           <ul className="space-y-4">
             {/* Partners Group */}
-            <li><div className="px-4 mb-1 text-xs font-semibold text-gray-500 uppercase">
+            {/* <li><div className="px-4 mb-1 text-xs font-semibold text-gray-500 uppercase">
               Партнеры
             </div>
-            <ul className="mt-1 space-y-1">
+            <ul className="mt-1 space-y-1"> */}
               {/* <SidebarItem
                 icon={<Users className="w-5 h-5 mr-3" />}
                 label="Партнеры"
                 href="/partners"
                 active={isActive("/partners")}
               /> */}
-              <SidebarItem
+              {/* <SidebarItem
                 icon={<Building2 className="w-5 h-5 mr-3" />}
                 label="Клиенты"
                 href="/clients"
                 active={isActive("/clients")}
               />
             </ul>
-          </li>
+          </li> */}
 
             {/* Clients Group */}
             <li>
